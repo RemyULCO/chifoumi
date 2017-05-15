242d32936c02dce953bc9012269d427ad72dff57
RemyULCO <mrheisenberg062@gmail.com>
2017-05-15 11:49:48 +0200

    tests unitaires close #3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <glog/logging.h>
 
-int main(){
+int main(int argc, char ** argv){
+  google::InitGoogleLogging(argv[0]);
+  google::SetLogDestination(google::GLOG_INFO, "log_chifoumi");
   std::cout << "Hello world"<<std::endl;
+  LOG(INFO) << "Test log hello world";
   return 0;
 }
