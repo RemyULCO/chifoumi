72c5208aea5207324000e3868f28b679e45a5b2d
RemyULCO <mrheisenberg062@gmail.com>
2017-05-15 16:59:03 +0200

    interface console close #8

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 #include <glog/logging.h>
 #include "Partie.hpp"
+#include "Console.hpp"
 
 int main(int argc, char ** argv){
-  google::InitGoogleLogging(argv[0]);
-  google::SetLogDestination(google::GLOG_INFO, "log_chifoumi");
-  Partie p;
-  int i=0;
-  while(i<3){
-    std::string joue;
-    std::cout << "paper,scissor,rock\n";
-    std::cin >> joue;
-    p.play("Remy",joue);
-    i++;
-  }
+  Console c;
+  c.run(argc,argv);
   return 0;
 }
